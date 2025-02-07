bf2c8e2ae11811148eddac307eaa3c2ff5487b5a
kronos2k4 <kronos2k4@gmail.com>
2025-02-07 14:22:49 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default {
   plugins: [
@@ -12,6 +16,16 @@ export default {
     })
   ],
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        example1: resolve(__dirname, 'example1.html'),
+        example2: resolve(__dirname, 'example2.html'),
+        example3: resolve(__dirname, 'example3.html'),
+        example4: resolve(__dirname, 'example4.html'),
+        example5: resolve(__dirname, 'example5.html'),
+      }
+    },
     sourcemap: true,
   }
 }
